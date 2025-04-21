7e22a75d3fd15ed9b66cbe2c554bf083de7c5472
QMODEM <kakros918@protonmail.com>
2025-04-21 17:35:07 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -11,6 +11,8 @@ function git_sparse_clone() {
 }
 
 # Add extra plugins 
+git clone --depth=1 https://github.com/QMODEM/telegrambot package/telegrambot
+git clone --depth=1 https://github.com/beralt/atinout package/atinout
 git clone --depth=1 https://github.com/QMODEM/quectel-CM-5G package/quectel-CM-5G
 git clone --depth=1 https://github.com/iFHax/luci-app-3ginfo-lite package/luci-app-3ginfo-lite
 git clone --depth=1 https://github.com/iFHax/luci-app-modem package/luci-app-modem
